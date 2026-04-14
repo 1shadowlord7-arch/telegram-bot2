6170b7c50b8098c1f7bff2f6744578d54efc03d0
1shadowlord7-arch <1shadowlord7@gmail.com>
2026-04-14 13:26:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+import nest_asyncio
+nest_asyncio.apply()
+
 import os
 from pyrogram import Client, filters
 
